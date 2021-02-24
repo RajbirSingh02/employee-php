220d1d5cb298e742366687ae00114b94cf8829f6
Rajbir02 <67579379+Rajbir02@users.noreply.github.com>
2021-02-24 21:50:30 +0700

    Removed hard coding

---
+++ web/config.php
@@ -0,0 +1,24 @@
+<?php
+
+// MySQL connection information
+
+// $servername = "localhost";
+// $username = "root";
+// $password = "admin";
+// $dbname = "employees";
+
+//$servername = "us-cdbr-east-03.cleardb.com";
+//$username="bf538b6a375e50";
+//$password="aea2be8c";
+//$dbname="heroku_6c5f9579891d89d";
+
+$url = parse_url(getenv("DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$dbname = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $dbname);
+?>
+
+
